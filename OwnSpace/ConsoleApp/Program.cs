0ba6d9aee1d43d72aa049eff59bbad701f365d58
1030716613 <1030716613@qq.com>
2019-01-07 16:17:55 +0800

    hello C#

---
+++ OwnSpace/ConsoleApp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //双斜杠是注释  代码不执行
+            //先写个hello world
+            Console.WriteLine("Hello C#");
+
+            Console.ReadLine();
+        }
+    }
+}
