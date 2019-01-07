874b4e04179c3a5802e5514187ae68f734c16549
1030716613 <1030716613@qq.com>
2019-01-07 18:09:52 +0800

    加法尝试20190107

--- OwnSpace/ConsoleApp/Program.cs
+++ OwnSpace/ConsoleApp/Program.cs
@@ -10,40 +10,29 @@ namespace ConsoleApp
     {
         static void Main(string[] args)
         {
-            //双斜杠是注释  代码不执行
-            //先写个hello world
-            //Console.WriteLine("Hello C#");
+            Console.WriteLine($"请输入数值a：");
 
-            //我先给你写个简单的东西  你对照着课程练习下
+            string stra = Console.ReadLine();
 
-            Console.Title = "匹配模式";
+            int a = Convert.ToInt32(stra);
 
-            //string是字符串的意思
-            string heros = "寒冰，德玛，光辉";
+            Console.WriteLine($"请输入数值b:");
 
-            Console.WriteLine($"请选择英雄：{heros}");
+            string strb = Console.ReadLine();
 
-            string hero = Console.ReadLine().ToString();
+            int b = Convert.ToInt32(strb);
 
-            if (hero == "寒冰")
-            {
-                Console.WriteLine("你选择了ADC");
-            }
-            else if (hero == "德玛")
-            {
-                Console.WriteLine("你选择了战士");
-            }
-            else if (hero == "光辉")
-            {
-                Console.WriteLine("你选择了AP");
-            }
-            else
-            {
-                Console.WriteLine("英雄不存在");
-            }
+            int result = AddMethod(a, b);
 
-            Console.WriteLine("按任意键退出");
+            Console.WriteLine($"运算结果：{a}+{b}={result}");
+
+
             Console.ReadKey();
+        }
+
+        public static int AddMethod(int a, int b)
+        {
+            return a + b;
         }
     }
 }
--- OwnSpace/ConsoleApp2/Program.cs
+++ OwnSpace/ConsoleApp2/Program.cs
@@ -9,36 +9,41 @@ namespace ConsoleApp2
     class Program
     {
         static void Main(string[] args)
-        {
-            //就可以在这里写代码了  你先用一个控制台就行了
+        {//在这边上右键一下 可以进入调试模式，按f10 就是代码一行一行的走 
+
+            //现在这样是 写死的  也可以写个方法 传进去值计算
+
+            Console.WriteLine($"请输入第一个数：");
+
+            //这是一个string类型的
+            string stra = Console.ReadLine();
+
+            //把string 转为int
+            int a = Convert.ToInt32(stra);
+
+            Console.WriteLine($"请输入第二个数：");
+
+            //这是一个string类型的
+            string strb = Console.ReadLine();
+
+            //把string 转为int
+            int b = Convert.ToInt32(strb);
+
+            //调用addmethod方法  按f11进入到这个方法
+            int result = AddMethod(a,b);
+
+            Console.WriteLine($"运算：{a}+{b}={result}");
 
 
-            Console.Title = "排位模式";
-            string heros = "法师 刺客 坦克 辅助";
-            Console.WriteLine($"英雄种类选择：{heros}");
-            string hero = Console.ReadLine().ToString();
-            if (hero == "法师")
-            {
-                Console.WriteLine("你选择了远程技能型英雄");
-            }
-            else if (hero == "刺客")
-            {
-                Console.WriteLine("你选择了突进型英雄");
-            }
-            else if (hero == "坦克")
-            {
-                Console.WriteLine("你选择了近战英雄");
-            }
-            else if (hero=="辅助")
-            {
-                Console.WriteLine("你选择了保护型英雄");
-            }
-            else
-            {
-                Console.WriteLine("英雄类型不存在");
-            }
-            Console.WriteLine("按任意键退出");
             Console.ReadKey();
         }
+
+        //这里写一个加法方法 当然你可以实现很多种算法
+        //这个方法需要两个int 型的入参，返回值也是个int
+        public static int AddMethod(int a,int b)
+        {
+            return a + b;
+        }
+
     }
 }
