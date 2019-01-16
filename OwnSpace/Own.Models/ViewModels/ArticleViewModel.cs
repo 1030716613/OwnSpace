05dc901e9314f012deb17203591f33821313cd65
1030716613 <1030716613@qq.com>
2019-01-16 22:10:26 +0800

    新加文章viewmodel

---
+++ OwnSpace/Own.Models/ViewModels/ArticleViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Own.Models
+{
+    public class ArticleViewModel
+    {
+        /// <summary>
+        /// 文章Id
+        /// </summary>
+        public int Id { get; set; }
+        public string NewsName { get; set; }
+        public string NewsAuthor { get; set; }
+        public string Abstract { get; set; }
+        public int NewsStatus { get; set; }
+        public string NewsImg { get; set; }
+        public string NewsLook { get; set; }
+        public string NewsTop { get; set; }
+        public string NewsTime { get; set; }
+        public string Content { get; set; }
+        public string NewsUrl { get; set; }
+    }
+}
