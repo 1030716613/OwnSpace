30d78797e42232bfb34e72902fedb7b26d59d6ad
cksun <1037134100@qq.com>
2019-01-17 18:50:17 +0800

    登录接口

--- OwnSpace/Own.Common/SqlHelper.cs
+++ OwnSpace/Own.Common/SqlHelper.cs
@@ -1,9 +1,6 @@
-﻿using System;
+﻿using Dapper;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Dapper;
 
 namespace Own.Common
 {
---
+++ OwnSpace/Own.MVC/Controllers/LoginController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Own.MVC.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public JsonResult VerifyLogin(Login login)
+        {
+            if (login.Name =="1" && login.PassWord =="1")
+            {
+                 var res = new { Status = "success", MsgData = "/Home" };
+                return Json(res);
+            }
+
+            return Json(new { Status = "false", MsgData = "用户名或密码不正确。" });
+        }
+    }
+
+    public class Login
+    {
+        public string Name { get; set; }
+
+        public string PassWord { get; set; }
+
+        public string Code { get; set; }
+    }
+}
