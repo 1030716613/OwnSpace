8358d0ac2d8a27b7a6ec99ba1c65a693ae3cc2f7
1030716613 <1030716613@qq.com>
2019-01-28 12:41:50 +0800

    C#111

--- OwnSpace/Cshapetest/Program.cs
+++ OwnSpace/Cshapetest/Program.cs
@@ -2,6 +2,7 @@
 using Own.Common;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,8 +11,10 @@ namespace Cshapetest
 {
     class Program
     {
-        static void Main()
+        static void Main(string[] arqs)
         {
+            #region 2019-01-19
+
             #region 2019-01-18
             //Console.WriteLine("输入第一个数：");
             //string str1 = Console.ReadLine();
@@ -38,10 +41,226 @@ namespace Cshapetest
             //Console.WriteLine(a1 + a2);
             #endregion
 
+            #region if else 的练习
 
-            Utils.TencentSmsMethod();
+            //string name;
 
+            //string a = "女";
+
+            //string b = "男";
+
+            //Console.WriteLine("输入姓名：");
+
+            //name = Console.ReadLine();
+
+            //if (name=="zhang")
+            //{
+            //    Console.WriteLine($"性别为{a}");
+
+            //}
+            //else if (name == "wang")
+            //{
+            //    Console.WriteLine($"性别为{b}");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("查无此人");
+            //}
+            #endregion
+
+            #region switch 的练习 break 跳出所有循环，continue 结束当前的这一次循环 继续循环下一个循环条件
+
+            //string name = null;
+
+            //var weekday = DateTime.Now.DayOfWeek.ToString();
+
+            //switch (weekday)
+            //{
+            //    case "Saturday":
+            //        name = "星期六";
+            //        break;
+
+            //    default:
+            //        break;
+            //}
+
+            //Console.WriteLine(name);
+
+
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    if (i==2)
+            //    {
+            //        break;
+            //    }
+            //    Console.WriteLine($"循环{i}");
+            //}
+
+            #endregion
+
+            #region a>b?a:b
+
+
+            //int a = int.Parse(Console.ReadLine());
+            //int b = int.Parse(Console.ReadLine());
+            //var istrue = a>b?a:b;
+            //Console.WriteLine($"{istrue}");
+
+            #endregion
+
+            #region 斐波那契数列
+
+            //int N = 10;
+            //int[] a = new int[N];
+            //a[0] = 1;
+            //a[1] = 1;
+            //for (int i = 2; i < N; i++)
+            //{
+            //    a[i] = a[i - 1] + a[i - 2];
+            //}
+            //for (int i = 0; i < N; i++)
+            //{
+            //    Console.WriteLine(a[i]);
+            //}
+
+            //递归
+            //int number = Foo(20);
+
+
+            #endregion
+
+            #region 生成随机数
+            //const int N = 50;
+            //int[] a = new int[N];
+            //Random b = new Random();
+            //for (int i = 0; i < N; i++)
+            //{
+            //    a[i] = b.Next() % 10;
+            //}
+            //foreach (int x in a)
+            //{
+            //    Console.WriteLine(x);
+            //}
+
+            #endregion
+
+            #region 冒泡排序
+
+            //int[] array = new int[] { 3, 5, 4, 2, 1, 9 };
+            //int temp;
+            //for (int i = 0; i < array.Length-1; i++)
+            //{
+            //    for (int j = i + 1; j < array.Length; j++)
+            //    {
+            //        if (array[i] > array[j])
+            //        {
+            //            temp = array[i];
+            //            array[i] = array[j];
+            //            array[j] = temp;
+            //        }
+            //    }
+            //}
+            //foreach (int item in array)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+            #endregion
+
+            #region List
+            ////大括号 对象初始化器
+            //var nums = new List<int>() { 2,5,9,4,6,5,2,78,45};
+            //nums.Add(12);
+            ////linq排序
+            //var numss  =  nums.OrderByDescending(t=>t);
+            //foreach (int item in numss)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+            #endregion
+
+            #region 递归查询文件数目
+            //    Console.WriteLine("正在统计中");
+
+            //    var count = FileOrDirCount("D:/2.DevelopTool");
+
+            //    Console.WriteLine(count);
+
+            //    Console.Read();
+            //}
+
+            //static long FileOrDirCount(string path)
+            //{
+            //    long count = 0;
+
+            //    try
+            //    {
+            //        var files = Directory.GetFiles(path);
+            //        count += files.Length;
+            //        //取文件夹
+            //        var dirs = Directory.GetDirectories(path);
+            //        foreach (var dir in dirs)
+
+            //        {
+
+            //            count += FileOrDirCount(dir);
+            //        }
+            //    }
+            //    catch (Exception ex)
+            //    {
+            //        Console.WriteLine(ex.Message);
+            //        //throw;
+            //    }
+            //    return count;
+            #endregion
+
+            #endregion
+
+            //double x = 100.5;
+            //double y = 0.02;
+            //var result = Interest(x, y);
+            //Console.WriteLine(result);
+
+
+      
             Console.Read();
         }
+
+
+
+
+
+
+
+        
+
+
+        /// <summary>
+        /// 递归求斐波那契数列
+        /// </summary>
+        //public static int Foo(int num)
+        //{
+        //    if (num <= 0)
+        //    {
+        //        return 0;
+        //    }
+
+        //    else if (num == 1 || num == 2)
+        //    {
+        //        return 1;
+        //    }
+
+        //    return Foo(num - 1) + Foo(num - 2);
+        //}
     }
+
+    public class Student
+    {
+        public string Name { get; set; }
+        public int Age{ get; set; }
+
+
+    }
+
 }
---
+++ OwnSpace/DashBorad/DashBorad.cs
@@ -0,0 +1,281 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DashBorad
+{
+
+    #region 仪表盘   封装
+    //class DashBorad  //仪表盘
+    //{
+    //    public string Name;  //表盘名称
+    //    private  double Value;  //表盘读数
+    //    public List<零件>零件s;
+
+    //    public double  GetValue()
+    //    {
+    //        return Value;
+    //    }
+
+    //    public void Rotate(double angle)  //指针转动
+    //    {
+    //        Value = angle;
+    //        Console.WriteLine("转动了{0}度",angle);
+    //    }
+
+    //    public void Reset()  //指针复位
+    //    {
+    //        Value = 0;
+    //        Console.WriteLine("表盘复位完成");
+    //    }
+
+    //    public void ShowValue()
+    //    {
+    //        Console.WriteLine("当前表盘读数{0}", Value);
+
+    //    }
+
+    //    public bool Save()   //将表盘读数存到数据库
+    //    {
+    //        数据库 dataBase = new 数据库();
+    //            return  dataBase.Store(this);6
+    //    }
+
+    //}
+
+    //class 零件 { }
+
+    //class 数据库
+    //{
+    //    public bool Store(DashBorad dashBorad)
+    //    {
+    //        // db.GetValue;
+    //        return true;
+    //    }
+    //}
+
+    //class Program
+    //{
+    //    static void Main(string[] args)
+    //    {
+    //        DashBorad db = new DashBorad();  //实例化
+
+    //        db.Name = "速度表盘";
+    //        db.Reset();
+
+    //        db.Rotate(10.0);
+
+    //        db.ShowValue();
+
+    //        Console.Read();
+
+    //    }
+
+    //}
+    #endregion
+    #region 继承
+
+
+    //class Program
+    //{
+    //    static void Main(string[] args)
+    //    {
+    //        var dog = new Dog() { Name = "小黑" };
+
+    //        dog.Shout();
+
+    //        var cat = new Cat() { Name = "小猫" };
+
+    //        cat.Shout();
+
+    //        Console.Read();
+    //    }
+    //}
+
+    ///// <summary>
+    ///// 父类 animal
+    ///// </summary>
+    //public class Animal
+    //{
+
+    //    /// 父类的属性
+    //    public string Name { get; set; }
+
+    //}
+
+    ///// <summary>
+    ///// 狗类 继承父类animal
+    ///// </summary>
+    //public class Dog : Animal
+    //{
+    //    public void Shout()
+    //    {
+    //        Console.WriteLine(this.Name + ":汪汪");
+    //    }
+    //}
+
+    ///// <summary>
+    ///// 猫类 继承父类animal
+    ///// </summary>
+    //public class Cat : Animal
+    //{
+    //    public void Shout()
+    //    {
+    //        Console.WriteLine(this.Name + ":喵喵");
+    //    }
+    //}
+
+    #endregion
+    #region 浮点运算精度问题
+
+
+    //static void Main(string[] args)
+    //{
+    //    double a = 3.876700875;
+
+    //    double b = 2.6;
+
+    //    double c = a-b;
+
+    //    var d= Math.Round(c,5);
+
+
+
+    //    Console.WriteLine(d.ToString("N5"));
+
+    //    Console.Read();
+    //}
+    #endregion
+    #region 抽象类继承
+
+    //class Program
+    //{
+    //    static void Main(string[] args)
+    //    {
+    //        Lead lead = new Lead();
+
+
+    //        RefreshBlood(lead.leadBlood);
+    //        RefreshBlood(lead.mercenary.mercenaryBlood);
+    //        Thread.Sleep(1000);  //战斗1秒
+
+    //        for (int i = 0; i < 12; i++)
+    //        {
+    //            lead.leadBlood.blood -= 30;
+    //            lead.mercenary.mercenaryBlood.blood -= 50;
+
+    //            RefreshBlood(lead.leadBlood);
+    //            RefreshBlood(lead.mercenary.mercenaryBlood);
+    //            Thread.Sleep(1000);  //战斗1秒
+    //            FloodTheScreen();
+    //        }
+
+    //        Console.Read();
+    //    }
+
+
+    //    /// <summary>
+    //    /// 刷新血条
+    //    /// </summary>
+    //    public static void RefreshBlood(DashBorad dashBorad)   //render
+    //    {
+    //        dashBorad.Render();
+    //    }
+
+    //    /// <summary>
+    //    /// 刷屏  这段代码写的没用到呀
+    //    /// 
+    //    /// </summary>
+    //    public static void FloodTheScreen()
+    //    {
+    //        for (int i = 1; i <= 2; i++)
+    //        {
+    //            Console.WriteLine();
+    //        }
+
+    //    }
+    //}
+
+    ///// <summary>
+    ///// 抽象类
+    ///// </summary>
+    //public abstract class DashBorad    //表盘   
+    //{
+    //    public abstract void Render();
+    //}
+
+
+    ///// <summary>
+    ///// 雇佣兵血量
+    ///// </summary>
+    //public class MercenaryBlood : DashBorad
+    //{
+    //    public int blood = 200;
+
+    //    public override void Render()
+    //    {
+    //        Console.WriteLine("雇佣兵的血量 为 {0}", this.blood);
+    //    }
+    //}
+
+    ///// <summary>
+    ///// 主角血量 
+    ///// </summary>
+    //public class LeadBlood : DashBorad
+    //{
+    //    public int blood = 100;
+
+    //    public override void Render()
+    //    {
+    //        Console.WriteLine("主角的血量 为 {0}", this.blood);
+    //    }
+    //}
+
+    ///// <summary>
+    ///// 主角
+    ///// </summary>
+    //public class Lead
+    //{
+    //    public LeadBlood leadBlood = new LeadBlood();
+
+    //    public Mercenary mercenary = new Mercenary();
+
+    //}
+
+    ///// <summary>
+    ///// 雇佣兵
+    ///// </summary>
+    //public class Mercenary
+    //{
+    //    public MercenaryBlood mercenaryBlood = new MercenaryBlood();
+    //}
+    #endregion
+
+    public class program
+    {
+
+
+        public static void Main(string[] args)
+        {
+
+
+
+
+
+
+            Console.Read();
+        }
+
+
+
+
+    }
+
+
+
+
+
+}
